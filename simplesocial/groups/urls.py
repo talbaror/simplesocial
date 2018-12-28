3d53cb4cb90492e7dc2514c2c967652987ae9486
talbaror <tbaror@gmail.com>
2018-12-28 21:24:35 +0200

    url and admin register

---
+++ simplesocial/groups/urls.py
@@ -0,0 +1,13 @@
+# GROUPS URL.PY 
+from django.conf.urls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns = [
+    url(r"^/$", views.ListGroups.as_view(), name="all"),
+    url(r"^new/$", views.CreatGroup.as_view(), name="create"),
+    url(r"^posts/in/(?P<slug>[-\w]+)/$", views.SingeleGroup.as_view(), name="single"),
+    
+    
+]
